18cf6905cecd623e86f9f1de0de0bde46dbbe156
DJ <davidji99@users.noreply.github.com>
2020-11-04 19:40:00 +0900

    address some golanglint

--- herokux/resource_herokux_oauth_authorization_test.go
+++ herokux/resource_herokux_oauth_authorization_test.go
@@ -80,24 +80,24 @@ func TestAccHerokuxOauthAuthorization_NoTTL(t *testing.T) {
 }
 
 func testAccCheckHerokuxOauthAuthorization_basic() string {
-	return fmt.Sprintf(`
+	return `
 resource "herokux_oauth_authorization" "foobar" {
 	scope = ["read"]
 	time_to_live = 100000
 	description = "This is an oauth authorization test from Terraform"
 }
-`)
+`
 }
 
 func testAccCheckHerokuxOauthAuthorization_CustomAPIKey() string {
-	return fmt.Sprintf(`
+	return `
 resource "herokux_oauth_authorization" "foobar" {
 	scope = ["read"]
 	auth_api_key_name = "TESTACC"
 	time_to_live = 100000
 	description = "This is an oauth authorization test from Terraform"
 }
-`)
+`
 }
 
 func testAccCheckHerokuxOauthAuthorization_NoTTL() string {
