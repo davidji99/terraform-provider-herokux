9ed0c7f05d57b5f0392f1fc04a1a88d178a91be9
DJ <davidji99@users.noreply.github.com>
2021-12-06 12:06:19 +0900

    Rename herokux_addons to herokux_app_addons (#191)
    * rename herokux_addons to herokux_app_addons
    
    * fix format in scheduler_job.md

--- herokux/data_source_herokux_app_addons_test.go
+++ herokux/data_source_herokux_app_addons_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestAccDatasourceHerokuxAddons_Basic(t *testing.T) {
+func TestAccDatasourceHerokuxAppAddons_Basic(t *testing.T) {
 	appID := testAccConfig.GetAppIDorSkip(t)
 	addonServiceName := ""
 
@@ -21,18 +21,18 @@ func TestAccDatasourceHerokuxAddons_Basic(t *testing.T) {
 				Config: testAccCheckHerokuxAddons_Basic(appID, addonServiceName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"data.herokux_addons.foobar", "app_id", appID),
+						"data.herokux_app_addons.foobar", "app_id", appID),
 					resource.TestCheckResourceAttr(
-						"data.herokux_addons.foobar", "addon_service_name", addonServiceName),
+						"data.herokux_app_addons.foobar", "addon_service_name", addonServiceName),
 					resource.TestCheckResourceAttrSet(
-						"data.herokux_addons.foobar", "addons"),
+						"data.herokux_app_addons.foobar", "addons"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccDatasourceHerokuxAddons_Filter(t *testing.T) {
+func TestAccDatasourceHerokuxAppAddons_Filter(t *testing.T) {
 	appID := testAccConfig.GetAppIDorSkip(t)
 	addonServiceName := "heroku-postgresql"
 
@@ -46,18 +46,18 @@ func TestAccDatasourceHerokuxAddons_Filter(t *testing.T) {
 				Config: testAccCheckHerokuxAddons_Basic(appID, addonServiceName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"data.herokux_addons.foobar", "app_id", appID),
+						"data.herokux_app_addons.foobar", "app_id", appID),
 					resource.TestCheckResourceAttr(
-						"data.herokux_addons.foobar", "addon_service_name", addonServiceName),
+						"data.herokux_app_addons.foobar", "addon_service_name", addonServiceName),
 					resource.TestCheckResourceAttrSet(
-						"data.herokux_addons.foobar", "addons"),
+						"data.herokux_app_addons.foobar", "addons"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccDatasourceHerokuxAddons_NotFound(t *testing.T) {
+func TestAccDatasourceHerokuxAppAddons_NotFound(t *testing.T) {
 	appID := testAccConfig.GetAppIDorSkip(t)
 	addonServiceName := "non-existent"
 
@@ -77,7 +77,7 @@ func TestAccDatasourceHerokuxAddons_NotFound(t *testing.T) {
 
 func testAccCheckHerokuxAddons_Basic(appID, addonServiceName string) string {
 	return fmt.Sprintf(`
-data "herokux_addons" "foobar" {
+data "herokux_app_addons" "foobar" {
   app_id = "%s"
   addon_service_name = "%s"
 }
