63da54bd816e640e0b56984e0cc1ee755b0a23d8
DJ <davidji99@users.noreply.github.com>
2025-08-15 16:18:22 +0900

    temp

--- api/customtime/customtime.go
+++ api/customtime/customtime.go
@@ -11,12 +11,30 @@ type CustomTime struct {
 
 const ctLayout = "2006/01/02|15:04:05"
 
+func parseFlexibleTime(s string) (time.Time, error) {
+	layouts := []string{
+		time.RFC3339,                // 2006-01-02T15:04:05Z07:00
+		"2006-01-02 15:04:05 -0700", // 2006-01-02 15:04:05 +0000
+		"2006-01-02 15:04:05 MST",   // 2006-01-02 15:04:05 UTC
+	}
+
+	var lastErr error
+	for _, layout := range layouts {
+		if t, err := time.Parse(layout, s); err == nil {
+			return t, nil
+		} else {
+			lastErr = err
+		}
+	}
+	return time.Time{}, lastErr
+}
+
 func (ct *CustomTime) UnmarshalJSON(data []byte) (err error) {
 	// Remove quotes from JSON string
 	str := string(data[1 : len(data)-1])
 
 	// Parse the custom format: "2024-07-31 01:16:17 +0000"
-	t, err := time.Parse("2006-01-02 15:04:05 -0700", str)
+	t, err := parseFlexibleTime(str)
 	if err != nil {
 		return err
 	}
--- api/general/mtls_iprule.go
+++ api/general/mtls_iprule.go
@@ -1,17 +1,13 @@
 package general
 
-import (
-	"github.com/davidji99/terraform-provider-herokux/api/customtime"
-)
-
 // MtlsIPRule represents a MTLS IP rule.
 type MtlsIPRule struct {
-	ID          *string                `json:"id,omitempty"`
-	CIDR        *string                `json:"cidr,omitempty"`
-	Description *string                `json:"description,omitempty"`
-	Status      *MTLSIPRuleStatus      `json:"status,omitempty"`
-	CreatedAt   *customtime.CustomTime `json:"created_at,omitempty"`
-	UpdatedAt   *customtime.CustomTime `json:"updated_at,omitempty"`
+	ID          *string           `json:"id,omitempty"`
+	CIDR        *string           `json:"cidr,omitempty"`
+	Description *string           `json:"description,omitempty"`
+	Status      *MTLSIPRuleStatus `json:"status,omitempty"`
+	//CreatedAt   *string           `json:"created_at,omitempty"`
+	//UpdatedAt   *string           `json:"updated_at,omitempty"`
 }
 
 // MTLSIPRuleRequest represents a request to create an IP rule.
